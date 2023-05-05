63681ede9fa365925bb5609108433dcff19dcdc3
laomoi <laomoi@qq.com>
2023-05-05 10:51:44 +0800

    sd root path 支持自动使用上层目录的stable-diffusion-webui

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 import sys
-sys.path.append("D:\stable-diffusion-webui-qingfeng")
+import os
+
+
+sd_root_path = ""
+
+
+
+parent_dir_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+if sd_root_path == "":
+    sd_root_path = os.path.join(parent_dir_path, 'stable-diffusion-webui')
+
+print("sd_root_path", sd_root_path)
+sys.path.append(sd_root_path)
